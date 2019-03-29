b84358f65f14a92d05922c82ab79855f9dbcc278
jhonatadev <xxxonata@gmail.com>
2019-03-29 05:39:53 -0300

    Percorrendo Array com ForEach

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -7,6 +7,18 @@ let data = [{
 }, {
 	id: 3, 
 	title: "Estudar JS"
+}, {
+	id: 4, 
+	title: "Estudar Nodejs"
 }];
 
-console.log(data);
+data.forEach(task => {
+	let li = document.createElement('li');
+
+	li.innerHTML = `
+		<input type="checkbox" id="task-${task.id}">
+		<label for="task-${task.id}">${task.title}</label>
+	`;
+
+	document.querySelector('.todo').appendChild(li);
+});
